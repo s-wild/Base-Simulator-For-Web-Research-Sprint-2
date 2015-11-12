f7c09bceec8362ce057018e2603d381630c6e89e
up637415 <up637415@myport.ac.uk>
2015-11-12 23:55:34 +0000

    Change canvas to draggable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,35 @@
-(function() {
-  canvas = this.__canvas = new fabric.StaticCanvas('battleField');
 
-  
-
-  canvas.add(
-    // new fabric.Rect({ top: 100, left: 100, width: 50, height: 50, fill: '#f55' })
-    //new fabric.Circle({ top: 140, left: 230, radius: 75, fill: 'green' }),
-    //new fabric.Triangle({ top: 300, left: 210, width: 100, height: 100, fill: 'blue' })
-  );
-
-  function animate() {
-    // canvas.item(0).animate('top', canvas.item(0).getTop() === 500 ? '100' : '500', { 
-    //   duration: 1000,
-    //   onChange: canvas.renderAll.bind(canvas),
-    //   onComplete: animate
-    // });
-  }
-  animate();
-})();
 
 $(document).ready(function() {
 
-    // Add tower click function.
-    $("#defence-tower").click(function() {
-      newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-        console.log("watch tower clicked.");
-        canvas.add(
-          new fabric.Rect({ top: Math.floor(Math.random() * 300) + 1  , left: Math.floor(Math.random() * 800) + 1, width: 200, height: 200, fill: newColor })
-        );
-    });
+  (function() {
+    canvas = new fabric.Canvas('Canvas');
+    fabric.Object.prototype.transparentCorners = false;
+    // canvas.on('mouse:over', function(e) {
+    //   e.target.setFill('red');
+    //   canvas.renderAll();
+    // });
+
+    // canvas.on('mouse:out', function(e) {
+    //   e.target.setFill('green');
+    //   canvas.renderAll();
+    // });
+
+  })();
+
+  // Add tower click function.
+  $("#defence-tower").click(function() {
+    newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+      console.log("watch tower clicked.");
+      localCirle = canvas.add(
+        new fabric.Circle({ top: Math.floor(Math.random() * 300) + 1  , left: Math.floor(Math.random() * 800) + 1, width: 200, height: 200, fill: newColor, radius: 50 })
+      );
+      localCirle.lockUniScaling = true;
+  });
 
 
     // Clear canvas function
-    $("#canvas-clear").click(function() {
+    $("#Canvas").click(function() {
         canvas.clear();
     });
 
