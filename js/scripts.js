c523b2e34f8f845612b9c873d3a45a92b9220d56
up637415 <up637415@myport.ac.uk>
2015-11-13 01:16:10 +0000

    Set up a background image for the map. Change opacity of shapes.

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
     newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
       console.log("watch tower clicked.");
       localCirle = canvas.add(
-        new fabric.Circle({ top: Math.floor(Math.random() * 600) + 1  , left: Math.floor(Math.random() * 800) + 1, width: 200, height: 200, fill: newColor, radius: 50, opacity: 0.5 })
+        new fabric.Circle({ top: Math.floor(Math.random() * 600) + 1  , left: Math.floor(Math.random() * 800) + 1, width: 200, height: 200, fill: newColor, radius: 50, opacity: 0.7 })
       );
       localCirle.lockUniScaling = true;
   });
@@ -34,5 +34,5 @@ $(document).ready(function() {
       console.log("clear it!");
   });
 
-    
+
 });
