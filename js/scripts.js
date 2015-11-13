c9a0b348087436bda3637f48b37670c1f5c8fb2b
up637415 <up637415@myport.ac.uk>
2015-11-13 01:53:18 +0000

    Added container and a base on control panel.

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,16 @@ $(document).ready(function() {
       localCirle.lockUniScaling = true;
   });
 
+  // Add base click function.
+  $("#base").click(function() {
+    newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+      console.log("watch tower clicked.");
+      localCirle = canvas.add(
+        new fabric.Rect({ top: Math.floor(Math.random() * 600) + 1  , left: Math.floor(Math.random() * 800) + 1, width: 200, height: 200, fill: newColor, opacity: 0.7, strokeWidth: 5, stroke: 'rgba(0,0,0,1)' })
+      );
+      localCirle.lockUniScaling = true;
+  });
+
 
   // Clear canvas function
   $("#canvas-clear").click(function() {
